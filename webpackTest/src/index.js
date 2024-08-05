ed8e186d45721ad39945808d5a49504165d0a936
Jeffrey Keem <keemaje5@gmail.com>
2024-08-04 20:02:26 -0700

    loading css, files, and data

--- webpackTest/src/index.js
+++ webpackTest/src/index.js
@@ -1,10 +1,22 @@
 import _ from 'lodash';
 import testM from './test';
+import './style.css';
+import cuteDog from './sarang.jpg';
 
 function component() {
-    const element = document.createElement('div');;
+    const element = document.createElement('div');
+    const para = document.createElement('p');
 
     element.textContent = testM('banana');
+    para.textContent = _.join(['hello', 'webpack'], ' ');
+    element.appendChild(para);
+
+    para.classList.add('notFruit');
+
+    const icon = new Image();
+    icon.src = cuteDog;
+
+    element.appendChild(icon);
 
     return element;
 }
--- webpackTest/webpack.config.js
+++ webpackTest/webpack.config.js
@@ -1,9 +1,34 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            // loading css, to add more, just put in use: 
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            // loading images
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            // loading data
+            // json files work by default
+            {
+                test: /\.(csv|tsv)$/i,
+                use: ['csv-loader'],
+            },
+            {
+                test: /\.xml$/i,
+                use: ['xml-loader'],
+            },
+        ],
     },
 };
