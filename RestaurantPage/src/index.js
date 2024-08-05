f871af163c2e0f67b6df726a8a05b7482959491d
Jeffrey Keem <keemaje5@gmail.com>
2024-08-04 23:30:24 -0700

    switching between pages

--- RestaurantPage/src/index.js
+++ RestaurantPage/src/index.js
@@ -1,1 +1,29 @@
-alert('Hi');
+import './styles.css';
+import makeHomeTab from './js/home';
+import makeMenuTab from './js/menu';
+import makeAboutTab from './js/about';
+
+function clearContent() {
+    const content = document.getElementById('content');
+    while (content.firstChild) {
+        content.removeChild(content.firstChild);
+    }
+}
+
+function switchTab(otherTab) {
+    clearContent();
+    const content = document.getElementById('content');
+    content.appendChild(otherTab());
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const homeButton = document.querySelector('button:nth-of-type(1)');
+    const menuButton = document.querySelector('button:nth-of-type(2)');
+    const aboutButton = document.querySelector('button:nth-of-type(3)');
+
+    homeButton.addEventListener('click', () => switchTab(makeHomeTab));
+    homeButton.addEventListener('click', () => switchTab(makeMenuTab));
+    homeButton.addEventListener('click', () => switchTab(makeAboutTab));
+
+    switchTab(makeHomeTab);
+});
